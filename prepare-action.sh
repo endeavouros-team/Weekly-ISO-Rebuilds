ca62dbb62851af8221a0ab4789d6a2af4ce2437c
Johannes Kamprad <archlinux@kamprad.net>
2023-02-12 21:51:54 +0100

    Update prepare-action.sh

--- prepare-action.sh
+++ prepare-action.sh
@@ -1,31 +1,25 @@
 #!/usr/bin/env bash
+tag=22.12.2.2
+
 # clone ISO sources and join the path:
-git clone https://github.com/endeavouros-team/EndeavourOS-ISO.git
+#git clone https://github.com/endeavouros-team/EndeavourOS-ISO.git
 
-# patch packages to not install Calamares packages from repository:
-#patch EndeavourOS-ISO/packages.x86_64 < packages.x86_64.patch
-
-# patch profiledef.sh -- set isoname to unstable:
-#patch EndeavourOS-ISO/profiledef.sh < profiledef.sh.patch
+#load tarball of theISO and unpack it
+wget https://github.com/endeavouros-team/EndeavourOS-ISO/archive/refs/tags/${tag}.tar.gz
+tar -xf ${tag}.tar.gz
 
 # patch run_before_squashfs.sh to remove github folder before squashfs:
-patch EndeavourOS-ISO/run_before_squashfs.sh < run_before_squashfs.sh.patch
-
-# copy live session Wallpaper into ISO structure:
-#cp livewall.png EndeavourOS-ISO/airootfs/root/
+patch EndeavourOS-ISO-${tag}/run_before_squashfs.sh < run_before_squashfs.sh.patch
 
 # run preperations inside ISO structure
-cd EndeavourOS-ISO
+cd EndeavourOS-ISO-${tag}
 
 # Copy packages from Build Calamares git packages into iso structure:
 #cp /home/build/packages/* airootfs/root/packages/
 
 # Get mirrorlist for offline installs
 mkdir "airootfs/etc/pacman.d"
-wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/mirrorlist"
-
-# Get wallpaper for installed system
-#wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/endeavouros-theming/master/backgrounds/endeavouros-wallpaper.png"
+wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist"
 
 # Make sure build scripts are executable
 chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
@@ -36,10 +30,6 @@ get_pkg() {
 }
 
 get_pkg "endeavouros-skel-xfce4"
-
-# current downgrade mesa for calamares lag in vms:
-# wget https://archive.archlinux.org/packages/m/mesa/mesa-22.1.7-1-x86_64.pkg.tar.zst
-# nmv mesa-22.1.7-1-x86_64.pkg.tar.zst "airootfs/root/packages/"
 
 chown -R build:build "airootfs/root/endeavouros-skel-liveuser"
 cd "airootfs/root/endeavouros-skel-liveuser"

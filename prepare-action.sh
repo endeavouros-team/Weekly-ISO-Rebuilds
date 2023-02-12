0a7d989325271318ff534b105f22f750e67953c1
Johannes Kamprad <archlinux@kamprad.net>
2023-02-12 21:40:51 +0100

    Update prepare-action.sh

--- prepare-action.sh
+++ prepare-action.sh
@@ -3,29 +3,29 @@
 git clone https://github.com/endeavouros-team/EndeavourOS-ISO.git
 
 # patch packages to not install Calamares packages from repository:
-patch EndeavourOS-ISO/packages.x86_64 < packages.x86_64.patch
+#patch EndeavourOS-ISO/packages.x86_64 < packages.x86_64.patch
 
 # patch profiledef.sh -- set isoname to unstable:
-patch EndeavourOS-ISO/profiledef.sh < profiledef.sh.patch
+#patch EndeavourOS-ISO/profiledef.sh < profiledef.sh.patch
 
 # patch run_before_squashfs.sh to remove github folder before squashfs:
 patch EndeavourOS-ISO/run_before_squashfs.sh < run_before_squashfs.sh.patch
 
 # copy live session Wallpaper into ISO structure:
-cp livewall.png EndeavourOS-ISO/airootfs/root/
+#cp livewall.png EndeavourOS-ISO/airootfs/root/
 
 # run preperations inside ISO structure
 cd EndeavourOS-ISO
 
 # Copy packages from Build Calamares git packages into iso structure:
-cp /home/build/packages/* airootfs/root/packages/
+#cp /home/build/packages/* airootfs/root/packages/
 
 # Get mirrorlist for offline installs
 mkdir "airootfs/etc/pacman.d"
 wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-ISO/main/mirrorlist"
 
 # Get wallpaper for installed system
-wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/endeavouros-theming/master/backgrounds/endeavouros-wallpaper.png"
+#wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/endeavouros-team/endeavouros-theming/master/backgrounds/endeavouros-wallpaper.png"
 
 # Make sure build scripts are executable
 chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}

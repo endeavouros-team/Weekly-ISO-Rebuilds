683a29c1f1d081183a5898c2cadae0975b59b81d
Johannes Kamprad <archlinux@kamprad.net>
2023-05-29 15:14:16 +0200

    Update add-EndeavourOS.sh

--- add-EndeavourOS.sh
+++ add-EndeavourOS.sh
@@ -2,7 +2,7 @@
 
 # setting up EndeavourOS repo:
 pacman -S --noconfirm --needed archlinux-keyring
-pacman-key --init && pacman-key --recv-key 003DB8B0CB23504F --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 003DB8B0CB23504F
+pacman-key --init && pacman-key --recv-key AED8858E4B9813F1 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key AED8858E4B9813F1
 cp  "pacman.conf" "/etc/pacman.conf"
 cp  "endeavouros-mirrorlist" "/etc/pacman.d/endeavouros-mirrorlist"
 pacman -Syy --noconfirm endeavouros-keyring endeavouros-mirrorlist

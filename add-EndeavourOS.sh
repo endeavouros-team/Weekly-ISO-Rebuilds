20c939796e9d7cc2487215a1f78a88ff49ecc12d
Johannes Kamprad <archlinux@kamprad.net>
2023-10-18 22:59:32 +0200

    Update add-EndeavourOS.sh

--- add-EndeavourOS.sh
+++ add-EndeavourOS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # setting up EndeavourOS repo:
-pacman -Sy --noconfirm --needed archlinux-keyring pipewire-jack
+pacman -Sy --noconfirm --needed archlinux-keyring
 cp  "pacman.conf" "/etc/pacman.conf"
 cp  "endeavouros-mirrorlist" "/etc/pacman.d/endeavouros-mirrorlist"
 pacman-key --init && pacman-key --recv-key 003DB8B0CB23504F --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 003DB8B0CB23504F

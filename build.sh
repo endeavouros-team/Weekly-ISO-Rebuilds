323a7ee271de61a0d22ff689d098b306e3b0339b
Johannes Kamprad <archlinux@kamprad.net>
2023-12-06 15:23:16 +0100

    [build.sh] packages.x86_64.patch

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cp "run_before_squashfs.sh.patch" "EndeavourOS-ISO/"
+cp "packages.x86_64.patch" "EndeavourOS-ISO/"
 cd "EndeavourOS-ISO"
 patch "run_before_squashfs.sh" < "run_before_squashfs.sh.patch"
+patch "packages.x86_64" < "packages.x86_64.patch"
 ./mkarchiso -v "." 2>&1 | tee "eosiso_$(date -u +'%Y.%m.%d-%H:%M').log"
 ls out/ > /tmp/isoname
 cd out/

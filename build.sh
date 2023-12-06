9c2f08df385e7fbeb2fc4098aee1d9fd65b70d8b
Johannes Kamprad <archlinux@kamprad.net>
2023-12-06 15:15:47 +0100

    [build.sh] add patch for kde online install rename kgamma5 > kgamma

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cp "run_before_squashfs.sh.patch" "EndeavourOS-ISO/"
 cd "EndeavourOS-ISO"
+patch "run_before_squashfs.sh" < "run_before_squashfs.sh.patch"
 ./mkarchiso -v "." 2>&1 | tee "eosiso_$(date -u +'%Y.%m.%d-%H:%M').log"
 ls out/ > /tmp/isoname
 cd out/

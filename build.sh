3317d0e668b91af16f8a59ba61c4424e92528fa2
Johannes Kamprad <archlinux@kamprad.net>
2023-12-12 18:38:54 +0100

    [build.sh] add torrent creation

--- build.sh
+++ build.sh
@@ -9,3 +9,4 @@ ls out/ > /tmp/isoname
 cd out/
 isoname=$(cat /tmp/isoname)
 sha512sum $isoname > $isoname.sha512sum
+mktorrent --announce=udp://tracker.openbittorrent.com:80 -a udp://tracker.torrent.eu.org:451/announce -a udp://thetracker.org:80/announce -a udp://tracker.dutchtracking.com:6969/announce -a udp://tracker.opentrackr.org:1337/announce -c $isoname.iso -n $isoname.iso -o $isoname.iso.torrent -v $isoname.iso -w http://www.endeavour.kamprad.net/weekly-iso/$isoname.iso

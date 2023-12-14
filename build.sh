671203152a3dd33e3dd7301ee76b27f659f2b4b8
Johannes Kamprad <archlinux@kamprad.net>
2023-12-14 13:58:44 +0100

    [build.sh] fix webseed url

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ ls out/ > /tmp/isoname
 cd out/
 isoname=$(cat /tmp/isoname)
 sha512sum $isoname > $isoname.sha512sum
-mktorrent --announce=udp://tracker.openbittorrent.com:80 -a udp://tracker.torrent.eu.org:451/announce -a udp://thetracker.org:80/announce -a udp://tracker.dutchtracking.com:6969/announce -a udp://tracker.opentrackr.org:1337/announce -c $isoname -n $isoname -o $isoname.torrent -v $isoname -w http://www.endeavour.kamprad.net/weekly-iso/$isoname
+mktorrent --announce=udp://tracker.openbittorrent.com:80 -a udp://tracker.torrent.eu.org:451/announce -a udp://thetracker.org:80/announce -a udp://tracker.dutchtracking.com:6969/announce -a udp://tracker.opentrackr.org:1337/announce -c $isoname -n $isoname -o $isoname.torrent -v $isoname -w https://endeavour.kamprad.net/weekly-iso/$isoname

d859d02b806442c488bd1b296d9f78e9a3273646
Johannes Kamprad <archlinux@kamprad.net>
2023-12-22 14:52:44 +0100

    [add-EndeavourOS.sh] addimng reflector ranking and fix key from info to manuel

--- add-EndeavourOS.sh
+++ add-EndeavourOS.sh
@@ -4,7 +4,9 @@
 pacman -Sy --noconfirm --needed archlinux-keyring
 cp  "pacman.conf" "/etc/pacman.conf"
 cp  "endeavouros-mirrorlist" "/etc/pacman.d/endeavouros-mirrorlist"
-pacman-key --init && pacman-key --recv-key 003DB8B0CB23504F --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 003DB8B0CB23504F
+pacman-key --init && pacman-key --recv-key 0F20FADC599D1C46EB556455AED8858E4B9813F1 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 0F20FADC599D1C46EB556455AED8858E4B9813F1
 pacman-key --init
-pacman -Syy --noconfirm endeavouros-keyring 
-pacman -Syy --noconfirm endeavouros-mirrorlist
+pacman -Sy --noconfirm endeavouros-keyring 
+pacman -S --noconfirm endeavouros-mirrorlist
+pacman -S reflector
+reflector --age 2 --number 23 --country DE,US --save /etc/pacman.d/mirrorlist

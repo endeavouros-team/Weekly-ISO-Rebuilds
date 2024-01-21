b1d49fc40cb1341b903de407dc53516882ceadf7
Johannes Kamprad <archlinux@kamprad.net>
2024-01-21 23:20:03 +0100

    [build.sh] add mkarchiso patch for license package changes

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 cp "run_before_squashfs.sh.patch" "EndeavourOS-ISO/"
 cp "packages.x86_64.patch" "EndeavourOS-ISO/"
+cp "mkarchiso.patch" "EndeavourOS-ISO/"
 cd "EndeavourOS-ISO"
 patch "run_before_squashfs.sh" < "run_before_squashfs.sh.patch"
 patch "packages.x86_64" < "packages.x86_64.patch"
+patch "mkarchiso" < "mkarchiso.patch"
 ./mkarchiso -v "." 2>&1 | tee "eosiso_$(date -u +'%Y.%m.%d-%H:%M').log"
 ls out/ > /tmp/isoname
 cd out/

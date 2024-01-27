32cfb790e3597576520b50533a6e4d484a7ca887
Johannes Kamprad <archlinux@kamprad.net>
2024-01-27 01:44:54 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #cp "run_before_squashfs.sh.patch" "EndeavourOS-ISO/"
 #cp "packages.x86_64.patch" "EndeavourOS-ISO/"
 #cp "mkarchiso.patch" "EndeavourOS-ISO/"
-#cd "EndeavourOS-ISO"
+cd "EndeavourOS-ISO"
 #patch "run_before_squashfs.sh" < "run_before_squashfs.sh.patch"
 #patch "packages.x86_64" < "packages.x86_64.patch"
 #patch "mkarchiso" < "mkarchiso.patch"
